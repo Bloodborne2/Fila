74518256e173e9c593d2d3d9fc0d9031a6be4509
Bloodborne2 <weeblete@gmail.com>
2025-03-14 21:35:28 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,66 @@
 public class Main {
+    static final int TAMANHO_MAXIMO = 100;
+
     public static void main(String[] args) {
-        FilaInteiro fila = new FilaInteiro(10);
-        fila.enqueue(10);
-        fila.ExibirFila();
-        fila.dequeue();
-        fila.ExibirFila();
+        Queue q = new Queue();
+        q.front = 0;
+        q.rear = 0;
 
-        fila.enqueue(103);
-        fila.ExibirFila();
+        System.out.println("Valor " + enqueue(q, 1) + " inserido na fila");
+        System.out.println("Valor " + enqueue(q, 2) + " inserido na fila");
+        System.out.println("Valor " + enqueue(q, 3) + " inserido na fila");
+        System.out.println("Valor " + enqueue(q, 4) + " inserido na fila\n");
 
-        fila.enqueue(120);
-        fila.ExibirFila();
+        System.out.println("Fila com " + (size(q) - 1) + " itens\n");
+        System.out.println("Proximo valor da fila " + front(q));
+        System.out.println("Tirando o valor " + dequeue(q) + " da fila ...\n");
 
-        fila.enqueue(150);
-        fila.ExibirFila();
+        System.out.println("Proximo valor da fila " + front(q));
+        System.out.println("Tirando o valor " + dequeue(q) + " da fila ...\n");
 
-        fila.dequeue(); // Sai o 103
-        fila.enqueue(110);
-        fila.ExibirFila();
+        System.out.println("Proximo valor da fila " + front(q));
+        System.out.println("Tirando o valor " + dequeue(q) + " da fila ...\n");
+
+        System.out.println("Proximo valor da fila " + front(q));
+        System.out.println("Tirando o valor " + dequeue(q) + " da fila ...\n");
+
+        System.out.println("Fila vazia, fila com " + (empty(q) - 1) + " itens\n");
+    }
+
+    static int empty(Queue pq) {
+        if (pq.front == pq.rear) {
+            return 1;
+        }
+        return 0;
+    }
+
+    static int enqueue(Queue pq, int x) {
+        if (pq.rear + 1 >= TAMANHO_MAXIMO) {
+            System.out.println("\nEstouro da capacidade da fila");
+        }
+        pq.itens[pq.rear++] = x;
+        return x;
+    }
+
+    static int size(Queue pq) {
+        return (pq.rear + 1);
+    }
+
+    static int front(Queue pq) {
+        return pq.itens[0];
+    }
+
+    static int dequeue(Queue pq) {
+        int x, i;
+        if (empty(pq) == 1) {
+            System.out.println("\nFila vazia");
+        }
+
+        x = pq.itens[0];
+        for (i = 0; i < pq.rear; i++) {
+            pq.itens[i] = pq.itens[i + 1];
+        }
+        pq.rear--;
+        return x;
     }
 }
-
